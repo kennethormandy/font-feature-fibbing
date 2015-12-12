1c3cd16333a900ba6f1cc660b8eb3cd8d1442cdc
Kenneth Ormandy <kenneth@chloi.io>
2015-12-12 15:40:04 -0800

    Removes versionify-ing for better ability to drop-in

--- src/index.js
+++ src/index.js
@@ -1,27 +1,21 @@
-/**
- * Font Feature Fibbing __VERSION__
- * https://github.com/kennethormandy/font-feature-fibbing
- * Copyright © 2014–2015 Kenneth Ormandy. Available under the MIT License.
- */
+module.exports = function () {
 
-module.exports = function() {
-
-  if(typeof document !== '') {
+  if (typeof document !== '') {
     // If font-feature-settings or -webkit-font-feature-settings are present
-    if(document.body.style['fontFeatureSettings'] === '' || document.body.style['webkitFontFeatureSettings'] === '') {
-      // Check if it’s Safari, then check if it’s version 8 or less
-      var ua = navigator.userAgent;
-      if(ua.match(/Safari/g) && ua.match(/Chrom/g) === null && ua.split('Version/').length > 1 && parseInt(ua.split('Version/')[1].split('.')[0], 10) <= 9) {
-        return false;
+    if (document.body.style['fontFeatureSettings'] === '' || document.body.style['webkitFontFeatureSettings'] === '') {
+      // Check if it’s Safari, then check if it’s version 9 or less
+      var ua = navigator.userAgent
+      if (ua.match(/Safari/g) && ua.match(/Chrom/g) === null && ua.split('Version/').length > 1 && parseInt(ua.split('Version/')[1].split('.')[0], 10) <= 9) {
+        return false
       } else {
         // Otherwise, font-feature-settings is supported in some capacity
-        return true;
+        return true
       }
     } else if (document.body.style['mozFontFeatureSettings'] === '' || document.body.style['MozFontFeatureSettings'] === '' || typeof document.body.style['msFontFeatureSettings'] === '') {
       // If font-feature-settings isn’t supported, we’re still good if -moz- or -ms- are here
-      return true;
+      return true
     }
-    return false;
+    return false
   }
 
 }
